00ad0144f6d55bc7cdd2514dd6c860e1f4a6ddba
Bùi Quốc Trọng <buiquoctrong110500@gmail.com>
2021-07-12 23:52:54 +0700

    Translator App

---
+++ translatorApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TranslatorappConfig(AppConfig):
+    name = 'translatorApp'
---
+++ translatorApp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+from translate import Translator
+
+# Create your views here.
+
+def home(request):
+    translation = ''
+    if request.method == 'POST':
+        text = request.POST["translate"]
+        language = request.POST["language"]
+        translator = Translator(to_lang = language)
+        translation = translator.translate(text)
+        # return translation
+    context = {
+        'translation': translation,
+    }
+    return render(request, "index.html", context)
+    
